c5d9c3e3d4ce15004f8215a4dce0dfc5603b8c92
IonutGrosu <ionutflorentin.grosu@gmail.com>
2021-04-14 08:39:23 +0200

    New Spring boot project

---
+++ ApplicationLogic/src/main/java/dk/via/sep3/group1/applicationlogic/ApplicationLogicApplication.java
@@ -0,0 +1,13 @@
+package dk.via.sep3.group1.applicationlogic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplicationLogicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApplicationLogicApplication.class, args);
+    }
+
+}
---
+++ ApplicationLogic/src/test/java/dk/via/sep3/group1/applicationlogic/ApplicationLogicApplicationTests.java
@@ -0,0 +1,13 @@
+package dk.via.sep3.group1.applicationlogic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationLogicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
