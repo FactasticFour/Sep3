230ebf882b8e05f3ef744824830877836876710f
IonutGrosu <ionutflorentin.grosu@gmail.com>
2021-04-14 08:33:34 +0200

    Deleted old java project

--- ApplicationLogic/src/main/java/HelloSep.java
+++
@@ -1,8 +0,0 @@
-public class HelloSep {
-    public static void main(String[] args) {
-        System.out.println("Roses are red"
-            + "\nMurder is wrong"
-            + "\nDBS is the best"
-            + "\nSuper duper ding dong");
-    }
-}
