a78c37604bff90804e3d830a5ed9d1699057271e
MariAsenova <mariaasenova22@gamil.com>
2021-04-26 08:06:25 +0200

    Model and DbContext class added

---
+++ PersistenceLayer/Persistence/Persistence/Data/UserContext.cs
@@ -0,0 +1,7 @@
+﻿namespace Persistence.Data
+{
+    public class UserContext
+    {
+        
+    }
+}
---
+++ PersistenceLayer/Persistence/Persistence/Models/User.cs
@@ -0,0 +1,7 @@
+﻿namespace Persistence.Models
+{
+    public class User
+    {
+        
+    }
+}
