9753d7d177f1aaa8e5bf3019c595d6a0b644e647
MariAsenova <mariaasenova22@gamil.com>
2021-04-26 07:15:43 +0200

    User model for generating initial migration create

---
+++ PersistanceLayer/Persistence/Persistence/Model/User.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Persistence.Model
+{
+    public class User
+    {
+        [Key] public int IdUser { get; set; }
+        [Required] public string Name { get; set; }
+        [Required] public string Password { get; set; }
+    }
+}
