825ff83f031cf52b02feb5c65fb1abd7a41c8f52
MariAsenova <mariaasenova22@gamil.com>
2021-04-28 13:52:24 +0200

    change id

--- ApplicationLogic/src/main/java/dk/via/sep3/group1/applicationlogic/ApplicationLogicApplication.java
+++ ApplicationLogic/src/main/java/dk/via/sep3/group1/applicationlogic/ApplicationLogicApplication.java
@@ -1,5 +1,7 @@
 package dk.via.sep3.group1.applicationlogic;
 
+import dk.via.sep3.group1.applicationlogic.networking.DataClient;
+import dk.via.sep3.group1.applicationlogic.networking.DataClientImpl;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,6 +10,9 @@ public class ApplicationLogicApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ApplicationLogicApplication.class, args);
+
+        DataClientImpl client = new DataClientImpl();
+        client.start();
     }
 
 }
--- ApplicationLogic/src/main/java/dk/via/sep3/group1/applicationlogic/model/dao/UserDAOImpl.java
+++ ApplicationLogic/src/main/java/dk/via/sep3/group1/applicationlogic/model/dao/UserDAOImpl.java
@@ -3,6 +3,8 @@ package dk.via.sep3.group1.applicationlogic.model.dao;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import dk.via.sep3.group1.applicationlogic.model.User;
+import dk.via.sep3.group1.applicationlogic.networking.DataClient;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.io.File;
@@ -14,8 +16,10 @@ public class UserDAOImpl implements UserDAO {
 
     private String usersFilePath = "users.json";
     private ArrayList<User> usersList;
-
     private ObjectMapper mapper;
+
+    @Autowired
+    DataClient dataClient;
 
     public UserDAOImpl() {
         mapper = new ObjectMapper();
@@ -34,12 +38,10 @@ public class UserDAOImpl implements UserDAO {
 
     @Override
     public User getUserById(int id) {
-        for (User user : usersList) {
-            if (user.getId() == id){
-                return user;
-            }
-        }
-
-        return null;
+        User user = dataClient.getUser(id);
+        if (user != null)
+            return user;
+        else
+            throw new RuntimeException("User null in DAO");
     }
 }
