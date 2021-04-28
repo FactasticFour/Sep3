ea96537069beb7aba2ed4bbaaee9d411b93abc9f
MariAsenova <mariaasenova22@gamil.com>
2021-04-28 10:11:24 +0200

    Db context

---
+++ PersistenceLayer/PersistenceServer/PersistenceServer/Data/DataContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PersistenceServer.Models;
+
+namespace PersistenceServer.Data
+{
+    public class DataContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseNpgsql("Host=hattie.db.elephantsql.com;Port=5432;Database=ezeateiy;Username=ezeateiy;Password=6uR8rdvQCc5r8Eho5SZbxjEcWhxtIUWE",
+                options => options.UseAdminDatabase("ezeateiy"));
+        }
+    }
+}
