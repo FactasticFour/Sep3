4a53e6f7f1a58cbc55daabe616c115f067c01734
MariAsenova <mariaasenova22@gamil.com>
2021-05-12 21:49:11 +0200

    Payload Argument for Request as Object

--- ApplicationLogic/src/main/java/dk/via/sep3/group1/applicationlogic/shared/Request.java
+++ ApplicationLogic/src/main/java/dk/via/sep3/group1/applicationlogic/shared/Request.java
@@ -3,8 +3,8 @@ package dk.via.sep3.group1.applicationlogic.shared;
 import java.io.Serializable;
 
 public class Request {
-    private String type;
-    private String argument; //TODO make enums
+    private String type; //TODO make enums
+    private Object argument;
 
     public Request(String type, String argument)
     {
@@ -16,7 +16,15 @@ public class Request {
         return type;
     }
 
-    public String getArgument() {
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public Object getArgument() {
         return argument;
+    }
+
+    public void setArgument(Object argument) {
+        this.argument = argument;
     }
 }
