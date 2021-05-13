b6e9e4eb1bcf5b650fa38dae1fa9cb6f886dac93
IonutGrosu <ionutflorentin.grosu@gmail.com>
2021-05-13 20:25:27 +0200

    Added Transfer Model class

---
+++ PersistenceLayer/PersistenceServer/PersistenceServer/Models/Transfer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace PersistenceServer.Models
+{
+    public class Transfer
+    {
+        //todo add transfer id?
+        [Required, Range(0, 9999999, ErrorMessage = "Invalid VIA ID")]
+        public int SenderViaId { get; set; }
+        
+        [Required, Range(0, 9999999, ErrorMessage = "Invalid VIA ID")]
+        public int ReceiverViaId { get; set; }
+
+        [Required, Range(0, 9000000)]
+        public int MoneyToTransfer { get; set; }
+        //todo rename this to amount or transferAmount?
+
+        [Required, Timestamp]
+        public DateTime Timestamp { get; set; }
+        //todo what about this one?
+        //https://www.c-sharpcorner.com/blogs/date-and-time-format-in-c-sharp-programming1
+    }
+}
