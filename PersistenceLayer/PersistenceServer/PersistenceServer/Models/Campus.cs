e67e0218f4d09d48ed1e64afd77a76235530e9a9
IonutGrosu <ionutflorentin.grosu@gmail.com>
2021-05-13 20:13:06 +0200

    Added Campus Model class

---
+++ PersistenceLayer/PersistenceServer/PersistenceServer/Models/Campus.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace PersistenceServer.Models
+{
+    public class Campus
+    {
+        //TODO what pk  here?
+        [Required, MaxLength(256)]
+        public string City { get; set; }
+
+        [Required, MaxLength(256)]
+        public string Street { get; set; }
+
+        [Required, StringLength(10, ErrorMessage = "Invalid postcode"), Column("postcode")]
+        public string PostCode { get; set; }
+
+        [Required, MaxLength(256)]
+        public string Name { get; set; }
+    }
+}
