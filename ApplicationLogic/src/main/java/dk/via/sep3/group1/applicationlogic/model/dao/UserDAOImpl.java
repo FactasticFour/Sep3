22b30897521c5fc4e33a286be836126ab84a896b
leppniz <leppniz@gmail.com>
2021-05-19 13:55:51 +0200

    Updating Annotation Service -> Repository

--- ApplicationLogic/src/main/java/dk/via/sep3/group1/applicationlogic/model/dao/UserDAOImpl.java
+++ ApplicationLogic/src/main/java/dk/via/sep3/group1/applicationlogic/model/dao/UserDAOImpl.java
@@ -4,11 +4,12 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import dk.via.sep3.group1.applicationlogic.model.User;
 import dk.via.sep3.group1.applicationlogic.networking.DataClient;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 
-@Service
+@Repository
 public class UserDAOImpl implements UserDAO {
 
     private String usersFilePath = "users.json";
