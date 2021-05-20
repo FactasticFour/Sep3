661dc868056f1877cfdd8d03585fb8f6f9581f2b
leppniz <leppniz@gmail.com>
2021-05-20 09:03:18 +0200

    Presentation Layer Update for CreditCard

---
+++ PresentationLayer/PresentationLayer/Data/ICreditCardService.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading.Tasks;
+using PresentationLayer.Models;
+
+namespace PresentationLayer.Data
+{
+    public interface ICreditCardService
+    {
+        Task AddCreditCardToAccount(CreditCard creditCard);
+    }
+}
---
+++ PresentationLayer/PresentationLayer/Data/Implementation/CreditCardService.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Net.Http;
+using System.Text.Json;
+using System.Threading.Tasks;
+using PresentationLayer.Models;
+
+namespace PresentationLayer.Data.Implementation
+{
+    public class CreditCardService : ICreditCardService
+    {
+        public async Task AddCreditCardToAccount(CreditCard creditCard)
+        {
+            HttpClient client = new HttpClient();
+            HttpResponseMessage responseMessage = await client.GetAsync($"http://localhost:8080/creditcards/{creditCard}");
+
+            if (!responseMessage.IsSuccessStatusCode)
+            {
+                throw new Exception($"Error: {responseMessage.StatusCode}, {responseMessage.ReasonPhrase}");
+            }
+            string result = await responseMessage.Content.ReadAsStringAsync();
+            
+        }
+    }
+}
