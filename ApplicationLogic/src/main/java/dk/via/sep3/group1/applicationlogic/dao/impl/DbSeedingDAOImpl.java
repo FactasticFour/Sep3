4a77328cba157526893afe0bb4b52ec0d89d77bf
MariAsenova <mariaasenova22@gamil.com>
2021-05-24 14:46:06 +0200

    dao outside

--- ApplicationLogic/src/main/java/dk/via/sep3/group1/applicationlogic/dao/impl/DbSeedingDAOImpl.java
+++ ApplicationLogic/src/main/java/dk/via/sep3/group1/applicationlogic/dao/impl/DbSeedingDAOImpl.java
@@ -1,6 +1,6 @@
-package dk.via.sep3.group1.applicationlogic.model.dao.impl;
+package dk.via.sep3.group1.applicationlogic.dao.impl;
 
-import dk.via.sep3.group1.applicationlogic.model.dao.DbSeedingDAO;
+import dk.via.sep3.group1.applicationlogic.dao.DbSeedingDAO;
 import dk.via.sep3.group1.applicationlogic.networking.DataClient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
--- ApplicationLogic/src/main/java/dk/via/sep3/group1/applicationlogic/service/impl/DbSeedingServiceImpl.java
+++ ApplicationLogic/src/main/java/dk/via/sep3/group1/applicationlogic/service/impl/DbSeedingServiceImpl.java
@@ -1,6 +1,6 @@
 package dk.via.sep3.group1.applicationlogic.service.impl;
 
-import dk.via.sep3.group1.applicationlogic.model.dao.DbSeedingDAO;
+import dk.via.sep3.group1.applicationlogic.dao.DbSeedingDAO;
 import dk.via.sep3.group1.applicationlogic.service.DbSeedingService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
