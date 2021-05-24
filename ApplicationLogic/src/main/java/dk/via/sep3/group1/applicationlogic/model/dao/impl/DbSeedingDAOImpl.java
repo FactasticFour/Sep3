de5846bbc2319cd638996635c9ee6b1372a0b6c2
MariAsenova <mariaasenova22@gamil.com>
2021-05-24 14:44:18 +0200

    dao impl package

--- ApplicationLogic/src/main/java/dk/via/sep3/group1/applicationlogic/model/dao/impl/DbSeedingDAOImpl.java
+++ ApplicationLogic/src/main/java/dk/via/sep3/group1/applicationlogic/model/dao/impl/DbSeedingDAOImpl.java
@@ -1,11 +1,12 @@
-package dk.via.sep3.group1.applicationlogic.model.dao;
+package dk.via.sep3.group1.applicationlogic.model.dao.impl;
 
+import dk.via.sep3.group1.applicationlogic.model.dao.DbSeedingDAO;
 import dk.via.sep3.group1.applicationlogic.networking.DataClient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public class DbSeedingDAOImpl implements DbSeedingDAO{
+public class DbSeedingDAOImpl implements DbSeedingDAO {
 
     @Autowired
     DataClient dataClient;
