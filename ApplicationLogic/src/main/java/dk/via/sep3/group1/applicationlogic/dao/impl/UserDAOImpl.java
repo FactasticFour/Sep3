4f123b2594ee07fa865d1d70fa2100bc00f8a84e
MariAsenova <mariaasenova22@gamil.com>
2021-05-24 21:49:55 +0200

    Clean up UserDao contructor from reading from a file

--- ApplicationLogic/src/main/java/dk/via/sep3/group1/applicationlogic/dao/impl/UserDAOImpl.java
+++ ApplicationLogic/src/main/java/dk/via/sep3/group1/applicationlogic/dao/impl/UserDAOImpl.java
@@ -1,14 +1,10 @@
 package dk.via.sep3.group1.applicationlogic.dao.impl;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import dk.via.sep3.group1.applicationlogic.model.User;
 import dk.via.sep3.group1.applicationlogic.dao.UserDAO;
 import dk.via.sep3.group1.applicationlogic.networking.DataClient;
 import dk.via.sep3.group1.applicationlogic.networking.DataClientImpl;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
-
-import java.util.ArrayList;
 
 @Repository
 public class UserDAOImpl implements UserDAO {
@@ -19,7 +15,7 @@ public class UserDAOImpl implements UserDAO {
 
     @Override
     public User getUserById(int id) {
-        // TODO remove autoWired and initialize the object everytime when you call method
+        // Initialize a new socket for every request/method call from DAOImpl
         DataClient dataClient = new DataClientImpl();
         User user = dataClient.getUser(id);
         if (user != null)
