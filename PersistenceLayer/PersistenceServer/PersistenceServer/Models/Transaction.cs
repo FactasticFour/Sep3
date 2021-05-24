5247ed7ca3226225ecdc31fa3f15f0290d7d9c4e
MariAsenova <mariaasenova22@gamil.com>
2021-05-24 12:08:23 +0200

    transaction class update - each tier

--- PersistenceLayer/PersistenceServer/PersistenceServer/Models/Transaction.cs
+++ PersistenceLayer/PersistenceServer/PersistenceServer/Models/Transaction.cs
@@ -1,14 +1,20 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace PersistenceServer.Models
 {
     public class Transaction
     {
+        [Key]
         public Account ReceiverAccount { get; set; }
+        [Key]
         public Account SenderAccount { get; set; }
+        [Required, ] 
         public float Amount { get; set; }
         public string Comment { get; set; }
+        [Required]
         public string Type { get; set; }
+        [Required]
         public DateTime Timestamp { get; set; }
 
         public Transaction(Account receiverAccount, Account senderAccount, string comment, string type, int payload, DateTime timestamp)
--- PresentationLayer/PresentationLayer/Models/Transaction.cs
+++ PresentationLayer/PresentationLayer/Models/Transaction.cs
@@ -1,14 +1,19 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace PresentationLayer.Models
 {
     public class Transaction
-    {
+    {   [Key]
         public Account ReceiverAccount { get; set; }
+        [Key]
         public Account SenderAccount { get; set; }
+        [Required, ] 
         public float Amount { get; set; }
         public string Comment { get; set; }
+        [Required]
         public string Type { get; set; }
+        [Required]
         public DateTime Timestamp { get; set; }
 
         public Transaction(Account receiverAccount, Account senderAccount, string comment, string type, int payload, DateTime timestamp)
