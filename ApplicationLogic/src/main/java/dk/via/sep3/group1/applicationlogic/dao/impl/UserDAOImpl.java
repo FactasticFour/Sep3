3dcc90938314063b79bcd481bd8fe53fd2f3a914
MariAsenova <mariaasenova22@gamil.com>
2021-05-24 21:55:49 +0200

    Merging working protocol to main

--- ApplicationLogic/src/main/java/dk/via/sep3/group1/applicationlogic/dao/impl/UserDAOImpl.java
+++ ApplicationLogic/src/main/java/dk/via/sep3/group1/applicationlogic/dao/impl/UserDAOImpl.java
@@ -1,16 +1,28 @@
 package dk.via.sep3.group1.applicationlogic.dao.impl;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import dk.via.sep3.group1.applicationlogic.model.User;
 import dk.via.sep3.group1.applicationlogic.dao.UserDAO;
 import dk.via.sep3.group1.applicationlogic.networking.DataClient;
 import dk.via.sep3.group1.applicationlogic.networking.DataClientImpl;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
 
 @Repository
 public class UserDAOImpl implements UserDAO {
 
-    public UserDAOImpl() {
+    private String usersFilePath = "users.json";
+    private ArrayList<User> usersList;
+    private ObjectMapper mapper;
 
+    @Autowired
+    DataClient dataClient;
+
+    public UserDAOImpl() {
+        mapper = new ObjectMapper();
+        usersList = new ArrayList<>();
     }
 
     @Override
@@ -24,4 +36,3 @@ public class UserDAOImpl implements UserDAO {
             throw new RuntimeException("User null in DAO");
     }
 }
-
