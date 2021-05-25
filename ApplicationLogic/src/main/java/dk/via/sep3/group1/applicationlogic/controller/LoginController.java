31b9a215ff500b190d1e2ec36e5d4df8577b714f
MariAsenova <mariaasenova22@gamil.com>
2021-05-25 20:33:38 +0200

    WIP: Rest Controller end point

--- ApplicationLogic/src/main/java/dk/via/sep3/group1/applicationlogic/controller/LoginController.java
+++ ApplicationLogic/src/main/java/dk/via/sep3/group1/applicationlogic/controller/LoginController.java
@@ -1,10 +1,36 @@
 package dk.via.sep3.group1.applicationlogic.controller;
 
+import dk.via.sep3.group1.applicationlogic.model.Account;
+import dk.via.sep3.group1.applicationlogic.service.LoginService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/users")
 public class LoginController {
 
+    @Autowired
+    LoginService loginService;
+
+
+    @GetMapping()
+    public Account validateUser(@RequestParam("username") String username, @RequestParam("password") String password) {
+        System.out.println("Controller - Username from client: " + username);
+        System.out.println("Controller - Password from client: " + password);
+
+        Account account = loginService.validateAccount(password, username);
+
+        // checking if caring default object
+        if (account.getAccountId() == 0) {
+            System.out.println("Role object is default, account not found");
+            return account;
+        } else {
+            System.out.println("Role to be sent to client: " + account.getAccountType() + "\n" + account.getAccountId() + "\n" + account.getViaEntity().getViaId());
+            return account;
+        }
+
+    }
 }
