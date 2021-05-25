9cc773cf9add61b1faaabe7a2dc374a0643bb17e
MariAsenova <mariaasenova22@gamil.com>
2021-05-25 21:55:39 +0200

    WIP: IAccount repository implementation

--- PersistenceLayer/PersistenceServer/PersistenceServer/Repository/Impl/AccountRepository.cs
+++ PersistenceLayer/PersistenceServer/PersistenceServer/Repository/Impl/AccountRepository.cs
@@ -11,16 +11,17 @@ namespace PersistenceServer.Repository.Impl
 
         public async Task<Account> GetAccountByUsernameAsync(string username)
         {
+            Console.WriteLine($"Username from handler: {username}");
             await using DataContext dataContext = new DataContext();
-            Account account = await dataContext.Accounts.Include(account => account.ViaEntity).Include(role => role.AccountType)
-                .FirstOrDefaultAsync(account => account.ViaEntity.ViaId.ToString().Equals(username));
 
-            Console.WriteLine($"Account found username:{account.ViaEntity.ViaId}");
+            Account account = await dataContext.Accounts.Include(c => c.ViaEntity).FirstOrDefaultAsync(a => a.AccountId == 77);
+
+            //Console.WriteLine($"Account found username:{account.ViaEntity.ViaId}");
             Console.WriteLine($"Account found AccountID:{account.AccountId}");
             Console.WriteLine($"Account found password:{account.ApplicationPassword}");
-
+            Console.WriteLine(account);
             //Role role = await dataContext.Roles.Include(roleTo => roleTo.Account).FirstOrDefaultAsync(roleTo => roleTo.Account.AccountId == include.AccountId);
-            Console.WriteLine($"Role found: {account.AccountType} + {account.ViaEntity.ViaId}");
+            //Console.WriteLine($"Role found: {account.AccountType} + {account.ViaEntity.ViaId}");
             return account;
         }
     }
