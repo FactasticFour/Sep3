b2b38f7708e3138d83c9e54d1a678dae3bd0c303
MariAsenova <mariaasenova22@gamil.com>
2021-05-25 20:05:48 +0200

    WIP: Authentication ILogin service added

---
+++ PresentationLayer/PresentationLayer/Authentication/CustomAuthenticationStateProvider.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Claims;
+using System.Text.Json;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.JSInterop;
+using PresentationLayer.Data;
+using PresentationLayer.Models;
+
+namespace PresentationLayer.Authentication
+{
+    public class CustomAuthenticationStateProvider : AuthenticationStateProvider
+    {
+        private readonly IJSRuntime jsRuntime;
+        private readonly ILoginService loginService;
+        private Account cachedUser;
+        
+        public CustomAuthenticationStateProvider(IJSRuntime jsRuntime, ILoginService loginService)
+        {
+            this.jsRuntime = jsRuntime;
+            this.loginService = loginService;
+        }
+        
+        public override async Task<AuthenticationState> GetAuthenticationStateAsync()
+        {
+            var identity = new ClaimsIdentity();
+            if (cachedUser == null)
+            {
+                string userAsJson = await jsRuntime.InvokeAsync<string>("sessionStorage.getItem", "currentUser");
+                if (!string.IsNullOrEmpty(userAsJson))
+                {
+                    cachedUser = JsonSerializer.Deserialize<Account>(userAsJson);
+                    identity = SetupClaimsForUser(cachedUser);
+                }
+            }
+            else
+            {
+                identity = SetupClaimsForUser(cachedUser);
+            }
+
+            ClaimsPrincipal cachedClaimsPrincipal = new ClaimsPrincipal(identity);
+            return await Task.FromResult(new AuthenticationState(cachedClaimsPrincipal));
+        }
+        
+        public async Task ValidateLogin(string password, string username)
+        {
+            Console.WriteLine("Validating log in");
+            if (string.IsNullOrEmpty(username)) throw new Exception("Enter username");
+            if (string.IsNullOrEmpty(password)) throw new Exception("Enter password");
+            ClaimsIdentity identity = new ClaimsIdentity();
+            try
+            {
+                Account account = await loginService.ValidateAccountAsync(password, username);
+                identity = SetupClaimsForUser(account);
+                string serialisedData = JsonSerializer.Serialize(account);
+                Console.WriteLine($"Serialized data: {serialisedData}");
+                await jsRuntime.InvokeVoidAsync("sessionStorage.setItem", "currentUser", serialisedData);
+                Console.WriteLine("after jsRuntime");
+                cachedUser = account;
+            }
+            catch (Exception e)
+            {
+                //throw e;
+                throw new Exception("Account not found check username and password");
+            }
+
+            NotifyAuthenticationStateChanged(Task.FromResult(new AuthenticationState(new ClaimsPrincipal(identity))));
+        }
+        
+        public async Task Logout()
+        {
+            cachedUser = null;
+            var user = new ClaimsPrincipal(new ClaimsIdentity());
+            await jsRuntime.InvokeVoidAsync("sessionStorage.setItem", "currentUser", "");
+            NotifyAuthenticationStateChanged(Task.FromResult(new AuthenticationState(user)));
+        }
+
+        private ClaimsIdentity SetupClaimsForUser(Account account)
+        {
+            List<Claim> claims = new List<Claim>();
+            claims.Add(new Claim(ClaimTypes.Name, account.AccountId.ToString()));
+            claims.Add(new Claim("Role", account.AccountType.RoleType));
+            ClaimsIdentity identity = new ClaimsIdentity(claims, "apiauth_type");
+            return identity;
+        }
+    }
+}
---
+++ PresentationLayer/PresentationLayer/Data/Implementation/LoginService.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Net.Http;
+using System.Text.Json;
+using System.Threading.Tasks;
+using PresentationLayer.Models;
+
+namespace PresentationLayer.Data.Implementation
+{
+    public class LoginService : ILoginService
+    {
+        public async Task<Account> ValidateAccountAsync(string username, string passwordHashed)
+        {
+            Console.WriteLine($"{username} + {passwordHashed}");
+            HttpClient client = new HttpClient();
+
+            HttpResponseMessage response =
+                await client.GetAsync($"http://localhost:8080/users?password={username}&username={passwordHashed}");
+            
+            if (!response.IsSuccessStatusCode)
+            {
+                throw new Exception($"Error: {response.StatusCode}, {response.ReasonPhrase}");
+            }
+            
+            string result = await response.Content.ReadAsStringAsync();
+            Console.WriteLine($"result before deserialization: {result}");
+            Account account = ToObject<Account>(result);
+            Console.WriteLine($"Account received: {account.AccountType.RoleType}");
+            // TODO handle rainy scenario
+            return account;
+        }
+        
+        private T ToObject<T>(String element)
+        {
+            var deserializeResult = JsonSerializer.Deserialize<T>(element, new JsonSerializerOptions()
+            {
+                PropertyNameCaseInsensitive = true
+            });
+            return deserializeResult;
+        }
+
+        private string ToJson<T>(T objToSerialize)
+        {
+            string serialized = JsonSerializer.Serialize(objToSerialize, new JsonSerializerOptions()
+            {
+                PropertyNamingPolicy = JsonNamingPolicy.CamelCase
+            });
+
+            return serialized;
+        }
+    }
+}
