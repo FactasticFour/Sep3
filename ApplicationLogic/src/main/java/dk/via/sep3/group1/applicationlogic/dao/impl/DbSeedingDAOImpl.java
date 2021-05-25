fef889a12a4d316c0c6d8ead0dfa554c88810a30
IonutGrosu <ionutflorentin.grosu@gmail.com>
2021-05-25 18:07:13 +0200

    Updated seeding

--- ApplicationLogic/src/main/java/dk/via/sep3/group1/applicationlogic/dao/impl/DbSeedingDAOImpl.java
+++ ApplicationLogic/src/main/java/dk/via/sep3/group1/applicationlogic/dao/impl/DbSeedingDAOImpl.java
@@ -3,6 +3,8 @@ package dk.via.sep3.group1.applicationlogic.dao.impl;
 import dk.via.sep3.group1.applicationlogic.dao.DbSeedingDAO;
 import dk.via.sep3.group1.applicationlogic.networking.DataClient;
 import dk.via.sep3.group1.applicationlogic.networking.DataClientImpl;
+import dk.via.sep3.group1.applicationlogic.shared.Reply;
+import dk.via.sep3.group1.applicationlogic.shared.Request;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
@@ -14,6 +16,14 @@ public class DbSeedingDAOImpl implements DbSeedingDAO {
 
     @Override
     public void seedDatabase() {
-
+        Request request = new Request();
+        request.setPayload(null);
+        request.setType(request.SEED_DATABASE);
+        Reply reply = dataClient.handleRequest(request);
+        if (reply.getType().equals(reply.SEEDING_SUCCESS)){
+            System.out.println("seeding was successful");
+        } else {
+            System.out.println("seeding was not successful");
+        }
     }
 }
