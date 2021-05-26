05f4cbad7218c871382416bffd5422cc7cbb4ae3
MariAsenova <mariaasenova22@gamil.com>
2021-05-26 14:03:50 +0200

    WIP: Rainy scenarios
    #Repository Account Impl

--- PersistenceLayer/PersistenceServer/PersistenceServer/Repository/Impl/AccountRepository.cs
+++ PersistenceLayer/PersistenceServer/PersistenceServer/Repository/Impl/AccountRepository.cs
@@ -16,16 +16,20 @@ namespace PersistenceServer.Repository.Impl
 
             Account account = await dataContext.Accounts.Include(c => c.ViaEntity).Include(a => a.AccountType)
                 .FirstOrDefaultAsync(a=> a.ViaEntity.ViaId.ToString().Equals(username));
-
             
-            Console.WriteLine($"Account found username:{account.ViaEntity.ViaId}");
-            Console.WriteLine($"Account found AccountID:{account.AccountId}");
-            Console.WriteLine($"Account found password:{account.ApplicationPassword}");
-            Console.WriteLine($"Account found with role:{account.AccountType.RoleType}");
-            Console.WriteLine(account);
-            //Role role = await dataContext.Roles.Include(roleTo => roleTo.Account).FirstOrDefaultAsync(roleTo => roleTo.Account.AccountId == include.AccountId);
-            //Console.WriteLine($"Role found: {account.AccountType} + {account.ViaEntity.ViaId}");
-            return account;
+            if (account.ViaEntity.ViaId.ToString().Equals(username))
+            {
+                Console.WriteLine($"Account found username:{account.ViaEntity.ViaId}");
+                Console.WriteLine($"Account found AccountID:{account.AccountId}");
+                Console.WriteLine($"Account found password:{account.ApplicationPassword}");
+                Console.WriteLine($"Account found with role:{account.AccountType.RoleType}");
+                Console.WriteLine(account);
+                return account;
+            }
+            else
+            {
+                throw new Exception("Account not found");
+            }
         }
     }
 }
