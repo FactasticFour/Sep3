94583ed946b6bdf2a41f178f240792df9bf0a94d
MariAsenova <mariaasenova22@gamil.com>
2021-05-26 14:06:38 +0200

    WIP: Rainy scenarios
    #Repository Account Impl

--- PersistenceLayer/PersistenceServer/PersistenceServer/Repository/Impl/AccountRepository.cs
+++ PersistenceLayer/PersistenceServer/PersistenceServer/Repository/Impl/AccountRepository.cs
@@ -8,28 +8,29 @@ namespace PersistenceServer.Repository.Impl
 {
     public class AccountRepository : IAccountRepository
     {
-
         public async Task<Account> GetAccountByUsernameAsync(string username)
         {
             Console.WriteLine($"Username from handler: {username}");
             await using DataContext dataContext = new DataContext();
-
-            Account account = await dataContext.Accounts.Include(c => c.ViaEntity).Include(a => a.AccountType)
-                .FirstOrDefaultAsync(a=> a.ViaEntity.ViaId.ToString().Equals(username));
+            Account account;
             
-            if (account.ViaEntity.ViaId.ToString().Equals(username))
+            try
             {
-                Console.WriteLine($"Account found username:{account.ViaEntity.ViaId}");
-                Console.WriteLine($"Account found AccountID:{account.AccountId}");
-                Console.WriteLine($"Account found password:{account.ApplicationPassword}");
-                Console.WriteLine($"Account found with role:{account.AccountType.RoleType}");
-                Console.WriteLine(account);
-                return account;
+                account = await dataContext.Accounts.Include(c => c.ViaEntity).Include(a => a.AccountType)
+                    .FirstOrDefaultAsync(a => a.ViaEntity.ViaId.ToString().Equals(username));
             }
-            else
+            catch (Exception e)
             {
-                throw new Exception("Account not found");
+                throw new Exception($"Account with username {username} could not be found");
             }
+
+            Console.WriteLine($"Account found username:{account.ViaEntity.ViaId}");
+            Console.WriteLine($"Account found AccountID:{account.AccountId}");
+            Console.WriteLine($"Account found password:{account.ApplicationPassword}");
+            Console.WriteLine($"Account found with role:{account.AccountType.RoleType}");
+            Console.WriteLine(account);
+            
+            return account;
         }
     }
 }
