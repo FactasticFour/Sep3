acf0b670070874c4bea9f1da54478cf8f3c606f0
MariAsenova <mariaasenova22@gamil.com>
2021-05-26 14:41:24 +0200

    WIP: rainy scenario application tier

--- ApplicationLogic/src/main/java/dk/via/sep3/group1/applicationlogic/controller/LoginController.java
+++ ApplicationLogic/src/main/java/dk/via/sep3/group1/applicationlogic/controller/LoginController.java
@@ -6,7 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping(value = "/login", method = RequestMethod.GET)
+@RequestMapping(value = "/login")
 public class LoginController {
 
     @Autowired
