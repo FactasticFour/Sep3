b5bb2e59c7269d04893b548930ec2438a99ec8d5
ClaudiuHVIA <65605206+ClaudiuHVIA@users.noreply.github.com>
2021-05-26 11:00:29 +0300

    Progress on deposit money on presentation layer and migrations deleted

---
+++ PresentationLayer/PresentationLayer/Data/ICreditCardService.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading.Tasks;
+
+namespace PresentationLayer.Data
+{
+    public interface ICreditCardService
+    {
+        public Task<bool> CheckCreditCard(int id);
+        public Task DepositMoney(float amount);
+    }
+}
---
+++ PresentationLayer/PresentationLayer/Data/Implementation/CreditCardService.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Net.Http;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace PresentationLayer.Data.Implementation
+{
+    public class CreditCardService : ICreditCardService
+    {
+        public async Task<bool> CheckCreditCard(int id)
+        {
+            HttpClient client = new HttpClient();
+            HttpResponseMessage responseMessage = await client.GetAsync($"http://localhost:8080/creditcard/{id}");
+            
+            if (!responseMessage.IsSuccessStatusCode)
+            {
+                throw new Exception($"Error: {responseMessage.StatusCode}, {responseMessage.ReasonPhrase}");
+            }
+
+            string result = await responseMessage.Content.ReadAsStringAsync();
+            
+            bool response = JsonSerializer.Deserialize<bool>(result, new JsonSerializerOptions
+            {
+                PropertyNameCaseInsensitive = true
+            });
+
+            return response;
+
+        }
+
+        public Task DepositMoney(float amount)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
